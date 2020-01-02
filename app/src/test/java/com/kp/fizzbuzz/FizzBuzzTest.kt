972f1a2e465b86b86337ec563383785e8638b589
karthickpandiyan <pandiyan289@gmail.com>
2020-01-02 11:15:11 +0530

    [Created test case] to test Given test function returns value when we pass Integer in argument

--- app/src/test/java/com/kp/fizzbuzz/FizzBuzzTest.kt
+++ app/src/test/java/com/kp/fizzbuzz/FizzBuzzTest.kt
@@ -1,8 +1,17 @@
 package com.kp.fizzbuzz
 
+import org.junit.Assert
+import org.junit.Test
+
 class FizzBuzzTest {
 
-    fun convert(convertTOFizzBuss: Int): String {
+    @Test
+    fun `Given test function returns value when we pass Integer in argument`(){
+        val value = "1"
+        Assert.assertEquals(value, convert(1))
+    }
+
+    private fun convert(convertTOFizzBuss: Int): String {
         return convertTOFizzBuss.toString()
     }
 }
