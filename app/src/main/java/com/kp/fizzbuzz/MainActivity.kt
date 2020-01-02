b284d63b8ec848d4af0491eeb25d94d1bb53779c
karthickpandiyan <pandiyan289@gmail.com>
2020-01-02 12:54:39 +0530

    Consume the usable solution and print it.

--- app/src/main/java/com/kp/fizzbuzz/MainActivity.kt
+++ app/src/main/java/com/kp/fizzbuzz/MainActivity.kt
@@ -13,5 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fizzBuzz = FizzBuzz()
+        for (i in 1..100){
+            System.out.println("$i = ${fizzBuzz.convert(i)}")
+        }
     }
 }
