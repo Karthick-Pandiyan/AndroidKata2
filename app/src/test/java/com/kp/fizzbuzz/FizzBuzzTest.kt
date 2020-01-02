9cfbe86cae4d4a80ab56ec2f40b7acf9f548ed32
karthickpandiyan <pandiyan289@gmail.com>
2020-01-02 11:16:52 +0530

    [Created test case] to test Given test function returns value when we pass Integer in argument - Checked false statement

--- app/src/test/java/com/kp/fizzbuzz/FizzBuzzTest.kt
+++ app/src/test/java/com/kp/fizzbuzz/FizzBuzzTest.kt
@@ -9,6 +9,7 @@ class FizzBuzzTest {
     fun `Given test function returns value when we pass Integer in argument`(){
         val value = "1"
         Assert.assertEquals(value, convert(1))
+        Assert.assertNotEquals(value, convert(2))
     }
 
     private fun convert(convertTOFizzBuss: Int): String {
